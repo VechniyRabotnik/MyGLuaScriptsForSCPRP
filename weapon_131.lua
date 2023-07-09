9b1c5f4dbd4bd0e5e8295d6827dc1b7f81207d9a
Vechniy Rabotnik <111284399+VechniyRabotnik@users.noreply.github.com>
2023-07-09 23:12:14 +0300

    Create weapon_131.lua

---
+++ weapon_131.lua
@@ -0,0 +1,86 @@
+SWEP.Author = "Вечный Работник"
+SWEP.Purpose = ""
+SWEP.Instructions = "ЛКМ - Пискнуть"
+SWEP.Category       = "Eternal | SWEPS"
+SWEP.Base = "weapon_base"
+SWEP.PrintName = "SCP 131"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.HoldType = "normal"
+SWEP.ViewModelFlip = false
+SWEP.ViewModel = ""
+SWEP.WorldModel = ""
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = true
+SWEP.ViewModelBoneMods = {}
+SWEP.Primary.ClipSize   = 0
+SWEP.Primary.DefaultClip = 0
+SWEP.Primary.Ammo = "none"
+SWEP.Secondary = false
+SWEP.Primary.Delay = 0
+SWEP.lastPrimaryUse = 0
+SWEP.ReloadCooldown = 20
+
+local a = "weapons/scp/131/pisk.wav" -- Локальная переменная для звука
+
+local cooldownDuration = 5 -- Длительность cooldown в секундах
+local cooldownEndTime = 0 -- Время окончания cooldown
+local drawCooldown = false -- Флаг для отображения cooldown
+
+function SWEP:Initialize()
+    self:SetHoldType(self.HoldType)
+    self:SetWeaponHoldType(self.HoldType)
+    
+    if CLIENT then
+        self:CallOnClient("HideViewModel")
+    end
+end
+
+function SWEP:Deploy()
+    if CLIENT then
+        self:CallOnClient("HideViewModel")
+    end
+end
+
+function SWEP:HideViewModel()
+    if not self:IsValid() then return end
+    self.Owner:GetViewModel():SetRenderMode(RENDERMODE_NONE)
+end
+
+function SWEP:PrimaryAttack()
+    if CurTime() < cooldownEndTime then return end
+    
+    self:EmitSound(a)
+    cooldownEndTime = CurTime() + cooldownDuration
+    drawCooldown = true
+end
+
+if CLIENT then
+    local pickIcon = Material("materials/scp/131/131.png")
+    function drawSCP131HUD()
+        local ply = LocalPlayer()
+        if not IsValid(ply) or not ply:Alive() then return end
+
+        local wep = ply:GetActiveWeapon()
+        if not IsValid(wep) or wep:GetClass() ~= "weapon_131" then return end
+
+        local x = ScrW() / 2 - 30 
+        local y = ScrH() - 140
+       -- local text = "Писк" -- text here
+    
+        -- Отрисовка первой иконки
+        surface.SetMaterial(pickIcon)
+        surface.SetDrawColor(255, 255, 255)
+        surface.DrawTexturedRect(x, y, 55, 55)
+        if drawCooldown and CurTime() < cooldownEndTime then
+         local cooldownPerc = (cooldownEndTime - CurTime()) / cooldownDuration
+         surface.SetDrawColor(20, 20, 20, 230)
+         surface.DrawRect(x, y, 55, 55)
+         surface.SetDrawColor(255, 255, 255, 130)
+         surface.DrawRect(x, y, 55, 55 * cooldownPerc)
+        else 
+           drawCooldown = false
+        end
+    end
+    hook.Add("HUDPaint", "SCP131HUD", drawSCP131HUD)
+end
